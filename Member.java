37c713199cd0f9dd4d8ff006bff9ffa647409c84
sujpatel <sujaypat3@gmail.com>
2025-01-16 12:59:23 -0500

    Add User class with attributes, constructor, and methods for borrowing, returning, and viewing books.

---
+++ Member.java
@@ -0,0 +1,23 @@
+public class Member extends User {
+    private int borrowLimit;
+
+    public Member(int id, String name) {
+        super(id, name);
+        this.borrowLimit = 5;
+    }
+
+    public int getBorrowLimit() {
+        return borrowLimit;
+    }
+
+    @Override
+    public void borrowBook(Book book) {
+        if (borrowedBooks.size() < borrowLimit) {
+            super.borrowBook(book);
+            System.out.println("Book borrowed successfully: " + book.getTitle());
+        } else {
+            System.out.println("Borrowing limit reached. You can only borrow up to " + borrowLimit + "books.");
+        }
+    }
+
+}
